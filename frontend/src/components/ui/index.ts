391da19a3a679aefbf9444521fb1e34fe8c8e1e4
isaaclins <isaac.lins07@gmail.com>
2025-07-17 13:47:02 +0200

    chore: restart simpler

--- frontend/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Input } from './Input';
-export { LoadingSpinner } from './LoadingSpinner';
-export { Modal } from './Modal';
-export { default as DrawingCanvas } from '../DrawingCanvas';
